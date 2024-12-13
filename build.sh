44d2f16cf27f6f6a0b77b0db91b0888d1b075afd
Aditya Garg <gargaditya08@live.com>
2024-12-13 12:50:24 +0530

    Use Official Kubuntu ISO

--- build.sh
+++ build.sh
@@ -1,96 +1,95 @@
 #!/bin/bash
+
 set -eu -o pipefail
 
-ROOT_PATH=$(pwd)
-WORKING_PATH=/root/work
-CHROOT_PATH="${WORKING_PATH}/chroot"
-IMAGE_PATH="${WORKING_PATH}/image"
-KERNEL_VERSION=6.11.8
+ROOT_PATH=$(pwd)/work
+OUTPUT_PATH=$(pwd)/output
+
+ISO_MOUNT_DIR="$ROOT_PATH/kubuntu-original"    # Temporary mount point for the original ISO
+VER=24.10
+CODENAME=oracular
+KERNEL_VERSION=6.11.7
 PKGREL=1
-sed -i "s/KVER/${KERNEL_VERSION}/g" $(pwd)/files/chroot_build.sh
-sed -i "s/PREL/${PKGREL}/g" $(pwd)/files/chroot_build.sh
+ISO_IMAGE=kubuntu-24.10-desktop-amd64.iso
+ISO_IMAGE_OUTPUT="${OUTPUT_PATH}/kubuntu-${VER}-${KERNEL_VERSION}-t2-${CODENAME}.iso"
+ISO_WORK_DIR="$ROOT_PATH/kubuntu-iso"
+CHROOT_DIR="$ROOT_PATH/kubuntu-edit"
 
-if [ -d "$WORKING_PATH" ]; then
-  rm -rf "$WORKING_PATH"
-fi
-mkdir -p "$WORKING_PATH"
-if [ -d "${ROOT_PATH}/output" ]; then
-  rm -rf "${ROOT_PATH}/output"
-fi
-mkdir -p "${ROOT_PATH}/output"
+echo "ROOT_PATH=$ROOT_PATH"
+echo "ISO_MOUNT_DIR=$ISO_MOUNT_DIR"  
+echo "ISO_WORK_DIR=$ISO_WORK_DIR"  
+echo "CHROOT_DIR=$CHROOT_DIR"
 
-echo >&2 "===]> Info: Build dependencies... "
-export DEBIAN_FRONTEND=noninteractive
-apt-get update
-apt-get install -y -qq -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" \
-  binutils \
-  debootstrap \
-  squashfs-tools \
-  xorriso \
-  grub-pc-bin \
-  grub-efi-amd64-bin \
-  mtools \
-  dosfstools \
-  zip \
-  isolinux \
-  syslinux
+mkdir -p "$ROOT_PATH"
+mkdir -p "$ISO_WORK_DIR"
+mkdir -p "$CHROOT_DIR"
+mkdir -p "$(dirname "$ISO_IMAGE_OUTPUT")"
+touch "$ISO_IMAGE_OUTPUT"
+echo >&2 "===]> Info: Installing required packages..."    
 
-echo >&2 "===]> Info: Start loop... "
-for ALTERNATIVE in t2-oracular
-do
-  echo >&2 "===]> Info: Start building ${ALTERNATIVE}... "
+apt update && apt update && \
+    DEBIAN_FRONTEND=noninteractive apt install -y tzdata \
+	&& apt install -y util-linux rsync squashfs-tools grub-pc-bin grub-common \
+	xorriso isolinux grub-efi-amd64-bin mtools dosfstools curl
 
-  echo >&2 "===]> Info: Build Kubuntu Oracular... "
-  /bin/bash -c "
+echo >&2 "===]> Info: Download ISO..."
+curl -L -o "$(pwd)/${ISO_IMAGE}" "https://cdimage.ubuntu.com/kubuntu/releases/${VER}/release/${ISO_IMAGE}"
+
+# Run entrypoint.sh to extract and customize the ISO
+echo >&2 "===]> Info: Starting extraction and customization..."
+/bin/bash -c "
+    ISO_IMAGE=${ISO_IMAGE} \\
+    ISO_MOUNT_DIR=${ISO_MOUNT_DIR} \\
+    ISO_WORK_DIR=${ISO_WORK_DIR} \\
+    CHROOT_DIR=${CHROOT_DIR} \\
     ROOT_PATH=${ROOT_PATH} \\
-    WORKING_PATH=${WORKING_PATH} \\
-    CHROOT_PATH=${CHROOT_PATH}_${ALTERNATIVE} \\
-    IMAGE_PATH=${IMAGE_PATH} \\
-    KERNEL_VERSION=${KERNEL_VERSION}-${PKGREL}-${ALTERNATIVE} \\
-    ALTERNATIVE=${ALTERNATIVE} \\
-    ${ROOT_PATH}/01_build_file_system.sh
-  "
+    KERNEL_VERSION=${KERNEL_VERSION} \\
+    $(pwd)/01_edit_iso.sh"
 
-  echo >&2 "===]> Info: Build Image Oracular... "
-  /bin/bash -c "
+# Enter the Chroot Environment and Apply Customizations
+echo >&2 "===]> Info: Creating chroot environment... "
+# Mount Required Filesystems for Chroot
+mount --bind /dev "${CHROOT_DIR}/dev"
+mount --bind /dev/pts "${CHROOT_DIR}/dev/pts"
+mount --bind /proc "${CHROOT_DIR}/proc"
+mount --bind /sys "${CHROOT_DIR}/sys"
+
+mkdir -p "${CHROOT_DIR}/tmp/setup_files"
+#rm -f "${CHROOT_DIR}/etc/resolv.conf"
+#make a back up
+cp -p "${CHROOT_DIR}/etc/resolv.conf" "${CHROOT_DIR}/etc/resolv.conf.backup"
+cp -p /etc/resolv.conf "${CHROOT_DIR}/etc/resolv.conf"
+cp "$(pwd)/chroot_iso.sh" "${CHROOT_DIR}/tmp/setup_files"
+ls "${CHROOT_DIR}/tmp/setup_files"
+echo >&2 "===]> Info: Running chroot environment... "
+chroot "${CHROOT_DIR}" /bin/bash -c "KERNEL_VERSION=${KERNEL_VERSION} PKGREL=${PKGREL} /tmp/setup_files/chroot_iso.sh"
+echo >&2 "===]> Info: Getting Kernel environment... "
+T2_KERNEL=${KERNEL_VERSION}-${PKGREL}-t2-${CODENAME}
+
+echo >&2 "===]> Info: Cleanup the chroot environment... "
+# restore backup
+cp -p "${CHROOT_DIR}/etc/resolv.conf.backup" "${CHROOT_DIR}/etc/resolv.conf"
+umount "${CHROOT_DIR}/dev/pts"
+umount "${CHROOT_DIR}/dev"
+umount "${CHROOT_DIR}/proc"
+umount "${CHROOT_DIR}/sys"
+
+echo >&2 "===]> Info: Squashing Kubuntu file system ... "
+mksquashfs "$CHROOT_DIR" "$ISO_WORK_DIR/casper/filesystem.squashfs" -comp xz -noappend
+
+# Run create_iso.sh to generate the new ISO
+# echo "Creating the custom ISO..."
+echo >&2 "===]> Info: Creating iso ... "
+/bin/bash -c "
+    ISO_WORK_DIR=${ISO_WORK_DIR} \\
+    CHROOT_DIR=${CHROOT_DIR} \\
+    ISO_IMAGE_OUTPUT=${ISO_IMAGE_OUTPUT} \\
     ROOT_PATH=${ROOT_PATH} \\
-    WORKING_PATH=${WORKING_PATH} \\
-    CHROOT_PATH=${CHROOT_PATH}_${ALTERNATIVE} \\
-    IMAGE_PATH=${IMAGE_PATH} \\
-    KERNEL_VERSION=${KERNEL_VERSION}-${PKGREL}-${ALTERNATIVE} \\
-    ALTERNATIVE=${ALTERNATIVE} \\
-    ${ROOT_PATH}/02_build_image.sh
-  "
+    T2_KERNEL=${T2_KERNEL} \\
+	$(pwd)/02_create_iso.sh"
+# split iso
 
-  echo >&2 "===]> Info: Prepare Boot for ISO... "
-  /bin/bash -c "
-    IMAGE_PATH=${IMAGE_PATH} \\
-    CHROOT_PATH=${CHROOT_PATH}_${ALTERNATIVE} \\
-    ${ROOT_PATH}/03_prepare_iso.sh
-  "
+split -b 1500M -x "${OUTPUT_PATH}/kubuntu-${VER}-${KERNEL_VERSION}-t2-${CODENAME}.iso" "${OUTPUT_PATH}/kubuntu-${VER}-${KERNEL_VERSION}-t2-${CODENAME}.iso."
+sha256sum "${OUTPUT_PATH}"/*.iso > "${OUTPUT_PATH}/sha256-kubuntu-${VER}"
 
-  echo >&2 "===]> Info: Create ISO... "
-  /bin/bash -c "
-    ROOT_PATH=${ROOT_PATH} \\
-    IMAGE_PATH=${IMAGE_PATH} \\
-    CHROOT_PATH=${CHROOT_PATH}_${ALTERNATIVE} \\
-    KERNEL_VERSION=${KERNEL_VERSION}-${ALTERNATIVE} \\
-    ALTERNATIVE=${ALTERNATIVE} \\
-    ${ROOT_PATH}/04_create_iso.sh
-  "
-  livecd_exitcode=$?
-  if [ "${livecd_exitcode}" -ne 0 ]; then
-    echo "Error building ${KERNEL_VERSION}-${ALTERNATIVE}"
-    exit "${livecd_exitcode}"
-  fi
-  ### Split iso into multiple parts - github max size of release attachment is 2GB, where ISO is sometimes bigger than that
-  cd "${ROOT_PATH}"
-  split -b 1500M -x "${ROOT_PATH}/kubuntu-24.10-${KERNEL_VERSION}-${ALTERNATIVE}.iso" "${ROOT_PATH}/output/kubuntu-24.10-${KERNEL_VERSION}-${ALTERNATIVE}.iso."
-done
-## Calculate sha256 sums of built ISO
-sha256sum "${ROOT_PATH}"/*.iso >"${ROOT_PATH}/output/sha256-kubuntu-24.10"
 
-find ./ | grep ".iso"
-#find ./ | grep ".zip"
-
-exit "${livecd_exitcode}"
--- build_in_docker.sh
+++ build_in_docker.sh
@@ -4,7 +4,9 @@ set -eu -o pipefail
 
 DOCKER_IMAGE=ubuntu:24.10
 
+echo "DOCKER_IMAGE=${DOCKER_IMAGE}"
 docker pull ${DOCKER_IMAGE}
+
 docker run \
   --privileged \
   --rm \
@@ -12,3 +14,4 @@ docker run \
   -v "$(pwd)":/repo \
   ${DOCKER_IMAGE} \
   /bin/bash -c 'cd /repo && ./build.sh'
+
